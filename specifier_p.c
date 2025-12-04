73a0d368eb8accceb2cddead18a82cc0c4d82e94
Aymane El Halkouj <aymel-ha@e1r6p12.1337.ma>
2025-12-04 16:09:31 +0100

    go

--- specifier_p.c
+++ specifier_p.c
@@ -6,11 +6,24 @@
 /*   By: aymel-ha <aymel-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 10:55:57 by aymel-ha          #+#    #+#             */
-/*   Updated: 2025/12/02 19:55:55 by aymel-ha         ###   ########.fr       */
+/*   Updated: 2025/12/04 15:40:07 by aymel-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "declarations.h"
+
+static size_t	specifier_x_p(unsigned long n)
+{
+	size_t	len;
+	char	*p;
+
+	len = 0;
+	p = "0123456789abcdef";
+	if (n >= 16)
+		len += specifier_x_p(n / 16);
+	len += specifier_c(p[n % 16]);
+	return (len);
+}
 
 size_t	specifier_p(void *pointer)
 {
@@ -20,6 +33,6 @@ size_t	specifier_p(void *pointer)
 		return (specifier_s("(nil)"));
 	len = 0;
 	len += specifier_s("0x");
-	len += specifier_x((unsigned long)pointer);
+	len += specifier_x_p((unsigned long)pointer);
 	return (len);
 }
